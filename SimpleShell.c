d220c418bc45717a8c24fa91579ca45de7b1a066
Mina Nabil <minanabil2004@gmail.com>
2025-02-27 01:44:27 +0200

    handle the input and finish the arguments

--- SimpleShell.c
+++ SimpleShell.c
@@ -1,55 +1,72 @@
+#include <sys/wait.h>
 #include <stdio.h>
 #include <stdbool.h>
+#include <stdlib.h>
 #include <signal.h>
 #include <unistd.h>
+//////////
 
-void main(){
-    signal(SIGINT, on_child_exit);
-    setup_environment();
-    shell();
-}
-
-void on_child_exit(){
-    reap_child_zombie()
-    write_to_log_file("Child terminated")
-}
-
+// void on_child_exit(){
+//     reap_child_zombie()
+//     write_to_log_file("Child terminated")
+// }
 
 void setup_environment(){
     chdir("/");
 }
 
-void shell(){
-    do{
-        char input[20];
-        scanf("%s", &input);
-        evaluate_expression():
-        switch(input_type):
-            case shell_builtin:
-                execute_shell_bultin();
-            case executable_or_error:
-                execute_command():
-
-    }
-    while 1;
-}
-
-void execute_shell_bultin(){
-    switch(command_type):
-        case cd:
-        case echo:
-        case export:
-}
-
-
-void execute_command(char command[], bool foreground){
+void execute_command(char *args[], bool foreground){
     pid_t pid = fork();
     if(pid == 0){
-        execvp(command,NULL);
-        print("Error");
-        exit();
+        execvp(args[0], args);
+        printf("Error");
+        exit(0);
     }
     else if(foreground){
-        waitpid(pid);
+        waitpid(pid, NULL, -1);
     }
+}
+
+void shell(){
+    do{
+        char command[100];
+        int i = 0;
+        char ch;
+        while (i < 99) {
+            scanf("%c", &ch);  
+            if (ch == '\n') { 
+                break;
+            }
+            command[i++] = ch;
+        }
+        command[i] = '\0';
+        char *args[10];
+        i = 0;
+        char *token = strtok(command, " ");
+        
+        while (token != NULL && i < 9) {
+            args[i++] = token;
+            token = strtok(NULL, " ");
+        }
+        args[i] = NULL;
+
+        switch(args[0]):
+            case "cd":
+                execute_shell_bultin(args);
+            case "echo":
+                execute_shell_bultin(args);
+            case "export":
+                execute_shell_bultin(args);
+            default:
+                execute_command(args):
+
+    }
+    while(true);
+}
+
+
+void main(){
+    //signal(SIGINT, on_child_exit);
+    setup_environment();
+    shell();
 }
