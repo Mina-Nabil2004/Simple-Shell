37bbe10e48ddd21b0bf91d021bce1b6978a0fa57
Mina Nabil <minanabil2004@gmail.com>
2025-03-01 19:43:56 +0200

    handles the exports

--- SimpleShell.c
+++ SimpleShell.c
@@ -6,7 +6,17 @@
 #include <signal.h>
 #include <unistd.h>
 #define MAX_INPUT 100
+#define MAX_VARIABLES 100
 #define MAX_ARGS 10
+
+
+typedef struct {
+    char name[MAX_INPUT/2 + 1];
+    char value[MAX_INPUT/2 + 1];
+} Variable;
+
+Variable *variables[MAX_VARIABLES];
+int nextVariable = 0;
 
 void execute_command(char **args, bool foreground){
     pid_t pid = fork();
@@ -22,8 +32,50 @@ void execute_command(char **args, bool foreground){
         exit(1);
     }
     else if(foreground){
-        waitpid(pid, NULL, -1);
+        waitpid(pid, NULL, 0);
     }
+}
+
+void export(char command[], Variable *newVariable){
+    int i;
+    bool flag = false;
+    newVariable->name[0] = '\0'; 
+    newVariable->value[0] = '\0';
+
+    for (int i = 7; i < MAX_INPUT; i++) { 
+        if (command[i] == '\0') break;  
+        if (command[i] == '=' && !flag) {
+            flag = true;
+            continue;
+        }
+        if (command[i] == '"') {
+            continue; 
+        }
+        if (!flag) {
+            strncat(newVariable->name, &command[i], 1); 
+        } else{
+            strncat(newVariable->value, &command[i], 1); 
+        }
+    }
+
+    variables[nextVariable++] = newVariable; 
+    printf("Stored Variable: Name: %s, Value: %s\n", newVariable->name, newVariable->value);
+}
+
+void echo(char **args){
+    
+}
+
+void parse(char command[], char *args[]){
+    int i = 0;
+    char *command_copy = strdup(command);
+    char *token = strtok(command_copy, " ");
+    
+    while (token != NULL && i < MAX_ARGS-1) {
+        args[i++] = token;
+        token = strtok(NULL, " ");
+    }
+    args[i] = NULL;
 }
 
 void shell(){
@@ -40,19 +92,16 @@ void shell(){
             command[i++] = ch;
         }
         command[i] = '\0';
-        char *args[MAX_ARGS];
-        i = 0;
-        char *token = strtok(command, " ");
-        
-        while (token != NULL && i < MAX_ARGS-1) {
-            args[i++] = token;
-            token = strtok(NULL, " ");
-        }
-        args[i] = NULL;
 
-        if (strcmp(args[0], "echo") == 0 || 
-            strcmp(args[0], "export") == 0) {
-            //execute_shell_builtin(args);
+        char *args[MAX_ARGS];
+        parse(command, args);
+        
+        if (strcmp(args[0], "export") == 0) {
+            Variable newVariable;
+            export(command, &newVariable);
+        }
+        else if(strcmp(args[0], "echo") == 0){
+            echo(args);
         }
         else if(strcmp(args[0], "cd") == 0){
             chdir(args[1]);
@@ -66,9 +115,10 @@ void shell(){
     while(true);
 }
 
-void on_child_exit(){
-    waitpid(-1, NULL, 0);
-    printf("Child terminated");
+void on_child_exit(int signo){
+    if(signo == SIGCHLD){
+        pid_t pid = waitpid(-1, NULL, WNOHANG);
+    }
 }
 
 void setup_environment(){
